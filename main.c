3feb0fc6f08f1955e0785307c357b7b87d13fb64
Ezequiel Salinas <cezsalinas@gmail.com>
2025-05-27 14:39:36 -0300

    cambios leves

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
     t_fecha fProceso;
 
     ingresarFechaProceso(&fProceso);
-    ///Prueba para ver si lee bien la fecha
+    ///Prueba para ver si lee bien la fecha ingresada
     //printf("La fecha es: %02d/%02d/%d", fechaProceso.dia, fechaProceso.mes, fechaProceso.anio);
 
     if(abrirArchivos(&fProceso) == TODO_OK)
